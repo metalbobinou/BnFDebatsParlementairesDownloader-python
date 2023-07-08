0def949b95490bf38a89bb233c60ff37d695da20
Metalman <metalbobinou@users.noreply.github.com>
2023-07-08 23:17:08 +0200

    CLI + dates
    - CLI management
    - dates correctly managed (and inversed when required)

--- src/1-debats_choose_range.py
+++ src/1-debats_choose_range.py
@@ -1,6 +1,36 @@
+# Argv
+import sys
+
+# Regexp
+import re
+
+# Date
 import datetime
-from datetime import date
-from dateutil.relativedelta import relativedelta
+
+# Prefix common to all parliamentary debates
+g_prefix_debates = "https://gallica.bnf.fr/ark:/12148/cb328020951/"
+
+# Example for debates of 5 february 1889
+#eg_url_targeted = "https://gallica.bnf.fr/ark:/12148/cb328020951/date18890205"
+
+##########################################################################
+## The objective of this module is to generate all the URLs for debates ##
+##########################################################################
+##
+## For example, to generate URL of debates between the 5 and 7 february 1889 :
+# python 1-debats_choose_range.py 1889-02-05 1889-02-07
+## ...it generates a file containing these :
+# "https://gallica.bnf.fr/ark:/12148/cb328020951/date18890205"
+# "https://gallica.bnf.fr/ark:/12148/cb328020951/date18890206"
+# "https://gallica.bnf.fr/ark:/12148/cb328020951/date18890207"
+##
+##########################################################################
+
+# Check arguments format
+def check_date_format(date_str):
+    # Expected Input : "1893-07-22"  --  "YYYY-mm-dd"
+    match = re.fullmatch("^[0-9]{4}-[0-9]{2}-[0-9]{2}$", date_str)
+    return (match != None)
 
 # Transform my format of date into a datetime object
 def transforms_date_to_datetime(input_date):
@@ -54,19 +84,65 @@ def generate_list_of_URL(first_date, last_date, url_prefix, url_suffix):
 
 # Write out a list into a file
 def write_list_to_file(input_list, filename):
+    # Clear file first
+    fd = open(filename, 'w')
+    fd.truncate(0)
+    fd.close()
+    # Then write out the list
     with open(filename, 'w') as fp:
         fp.write('\n'.join(input_list))
     fp.close()
 
+# Check for arguments in the CLI
+def main():
+    # Check for missing arguments
+    if (len(sys.argv) != 3):
+        print("Missing parameters")
+        print("")
+        print("Usage: " + sys.argv[0] + " first_date last_date")
+        print("")
+        print("Dates format: YYYY-mm-dd  (e.g.: 1893-07-22)")
+        return (-1)
+    else:
+        # Check for incorrect date format in one of the argument
+        if ((not (check_date_format(sys.argv[1]))) or
+            (not (check_date_format(sys.argv[2])))):
+            print("Incorrect date format")
+            print("")
+            print("Usage: " + sys.argv[0] + " first_date last_date")
+            print("")
+            print("Dates format: YYYY-mm-dd  (e.g.: 1893-07-22)")
+            return (-2)
+
+        # If everything is good, let's process data !
+        first_date = sys.argv[1]
+        last_date = sys.argv[2]
+        prefix_url = g_prefix_debates + "date"
+        suffix_url = ""
+
+        # Reverse dates if in incorrect order
+        if (transforms_date_to_datetime(first_date) >
+            transforms_date_to_datetime(last_date)):
+            tmp = first_date
+            first_date = last_date
+            last_date = tmp
+
+        url_list = generate_list_of_URL(first_date, last_date,
+                                        prefix_url, suffix_url)
+        filename = "list_url_" + first_date + "_"  + last_date + ".txt"
+        write_list_to_file(url_list, filename)
+        return (0)
+
+main()
 
 # MAIN OF TEST
-def main():
+def main_test():
     first_date = "1889-11-12"
     last_date = "1893-07-22"
     #last_date = "1893-10-14"
 
-    prefix_debates = "https://gallica.bnf.fr/ark:/12148/cb328020951/"
     url_targeted = "https://gallica.bnf.fr/ark:/12148/cb328020951/date18890205"
+    prefix_debates = "https://gallica.bnf.fr/ark:/12148/cb328020951/"
     prefix_date = "date"
     suffix_date = ""
 
