5c6a7ca2c1ae0fd1d95c2dacdc69250eec220882
Metalman <metalbobinou@users.noreply.github.com>
2023-07-19 19:34:05 +0200

    Update verification_step2.sh

--- check/verification_step2.sh
+++ check/verification_step2.sh
@@ -9,7 +9,7 @@ cat ${RESOLVED} ${COMPLEX} ${UNRESOLVED} > v_res-com-unr_1.txt
 cat v_res-com-unr_1.txt | awk -F " " '{ print $1}' > v_res-com-unr_2.txt
 cat v_res-com-unr_2.txt | sort > v_res-com-unr_3.txt
 # Remove lines beginning with '#'
-cat v_res-com-unr_3.txt | sed -n '/^\#/!p' > v_res-com-unr_3.txt
+cat v_res-com-unr_3.txt | sed -n '/^\#/!p' > v_res-com-unr_4.txt
 
 cat ${LIST} > v_liste_1.txt
 cat v_liste_1.txt | awk -F " " '{ print $1}' > v_liste_2.txt
