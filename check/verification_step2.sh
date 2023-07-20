718f92345c1f59abf586f77b7ba3c13fa149da96
fabrice.boissier <fabrice.boissier@epita.fr>
2023-07-20 16:50:28 +0200

    Correction on verification script

--- check/verification_step2.sh
+++ check/verification_step2.sh
@@ -6,11 +6,11 @@ COMPLEX="complex_*"
 EXTERNA="external2_*"
 UNRESOLVED="unresolved_*"
 
-cat ${RESOLVED} ${COMPLEX} ${EXTERNAL} ${UNRESOLVED} > v_res-com-ext-unr_1.txt
-cat v_res-com-ext-unr_1.txt | awk -F " " '{ print $1}' > v_res-com-ex-unr_2.txt
-cat v_res-com-ext-unr_2.txt | sort > v_res-com-ext-unr_3.txt
+cat ${RESOLVED} ${COMPLEX} ${EXTERNAL} ${UNRESOLVED} > v_RCEU_1.txt
+cat v_RCEU_1.txt | awk -F " " '{ print $1}' > v_RCEU_2.txt
+cat v_RCEU_2.txt | sort > v_RCEU_3.txt
 # Remove lines beginning with '#'
-cat v_res-com-ext-unr_3.txt | sed -n '/^\#/!p' > v_res-com-ext-unr_4.txt
+cat v_RCEU_3.txt | sed -n '/^\#/!p' > v_RCEU_4.txt
 
 cat ${LIST} > v_liste_1.txt
 cat v_liste_1.txt | awk -F " " '{ print $1}' > v_liste_2.txt
@@ -18,4 +18,4 @@ cat v_liste_2.txt | sort > v_liste_3.txt
 # Remove lines beginning with '#'
 cat v_liste_3.txt | sed -n '/^\#/!p' > v_liste_4.txt
 
-diff v_liste_4.txt v_res-com-ext-unr_4.txt
+diff v_liste_4.txt v_RCEU_4.txt
